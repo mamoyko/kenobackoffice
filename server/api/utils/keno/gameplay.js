8351aa6dbe00449d3a149c0bed2eff8854f99b27
Joel Ralph Balignasay <joelralph06@gmail.com>
2020-04-08 14:59:12 +0800

    chnages on api

--- server/api/utils/keno/gameplay.js
+++ server/api/utils/keno/gameplay.js
@@ -7,7 +7,11 @@ const getResponseObj = async (data) => {
     const matchedNumbers = await getMatches(arrDrawn, data.ballSelected);
     const reward = await getWinAmount(data.ballSelected.length, matchedNumbers.length, data.betAmount);
     const player = await PlayerModel.findById(data.id);
-    player.balance = player.balance - data.betAmount;
+    if (reward > 0){
+      player.balance = player.balance + data.betAmount;
+    } else{
+      player.balance = player.balance - data.betAmount;
+    }
     let newPlayer = await PlayerModel.findOneAndUpdate({_id : data.id}, player);
     return { 
         data: {
